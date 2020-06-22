e2f16cd4714bf5ec5cacad77c16d4f0845b36b39
Keneth Ade <kenjiwebdev@gmail.com>
2020-06-22 11:31:17 +0800

    Updates

---
+++ precache-manifest.91ace4da4e47a4a72287d1b7f556b95a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca692d865008eb7481c4ff18486acdba",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e171178a1f9df6573187",
+    "url": "/robofriends/static/css/main.1ab891b6.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/robofriends/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e171178a1f9df6573187",
+    "url": "/robofriends/static/js/main.a833ed16.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  },
+  {
+    "revision": "e9ee108cd098321ac27b95ebcebfcdf6",
+    "url": "/robofriends/static/media/Audiowide-Regular.e9ee108c.ttf"
+  }
+]);
